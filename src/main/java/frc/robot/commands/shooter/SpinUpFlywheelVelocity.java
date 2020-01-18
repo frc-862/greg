1fec45d386c0e12d19b453b38333558a6853dea1
Shane Hurley <shane@hurleyhome.com>
2020-01-17 22:58:51 -0500

    This is my code

---
+++ src/main/java/frc/robot/commands/shooter/SpinUpFlywheelVelocity.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.shooter;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class SpinUpFlywheelPower extends CommandBase {
+
+  Shooter shooter;
+  DoubleSupplier pwr;
+
+  /**
+   * Creates a new Fire.
+   */
+  public SpinUpFlywheelPower(Shooter shooter, DoubleSupplier pwr) {
+
+    this.shooter = shooter;
+    this.pwr = pwr;
+
+    addRequirements(shooter);
+
+    // Use addRequirements() here to declare subsystem dependencies...
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    shooter.setPower(pwr.getAsDouble());
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  
+    super.end(interrupted);
+    shooter.stop();
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

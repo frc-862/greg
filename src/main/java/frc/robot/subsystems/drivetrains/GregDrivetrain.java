a8bc5481ffeb41ea0b850f29a9cca5f839f0cd4a
Shane Hurley <shane@hurleyhome.com>
2020-02-19 16:27:19 -0500

    First pass at using encoders in drive train, and safety setting on sparkmax init

--- src/main/java/frc/robot/subsystems/drivetrains/GregDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrains/GregDrivetrain.java
@@ -7,32 +7,53 @@
 
 package frc.robot.subsystems.drivetrains;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-import com.revrobotics.ControlType;
-import com.revrobotics.CANSparkMax.IdleMode;
-
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.lightning.logging.DataLogger;
+import edu.wpi.first.wpilibj.Encoder;
 import frc.lightning.subsystems.NeoDrivetrain;
 import frc.lightning.util.RamseteGains;
 import frc.robot.Constants;
+import frc.robot.RobotConstants;
 import frc.robot.RobotMap;
 import frc.robot.misc.REVGains;
 
-import java.util.function.Consumer;
-
 public class GregDrivetrain extends NeoDrivetrain {
+    Encoder leftEncoder = new Encoder(0,1);
+    Encoder rightEncoder = new Encoder(2,3);
+
+    // needs to return meters
+    @Override
+    public double getLeftDistance() {
+        return leftEncoder.getDistance();
+    }
+
+    // needs to return meters
+    @Override
+    public double getRightDistance() {
+        return rightEncoder.getDistance();
+    }
+
+    // needs to return meters/sec
+    @Override
+    public double getLeftVelocity() {
+        return leftEncoder.getRate();
+    }
+
+    // needs to return meters/sec
+    @Override
+    public double getRightVelocity() {
+        return rightEncoder.getRate();
+    }
 
     public GregDrivetrain() {
         super(3, RobotMap.LEFT_1_CAN_ID, RobotMap.RIGHT_1_CAN_ID, Constants.GREG.getTrackWidth(), Constants.GREG);
         initMotorDirections();
 
+        leftEncoder.setDistancePerPulse(RobotConstants.ENCODER_PULSE_TO_METERS);
+        rightEncoder.setDistancePerPulse(RobotConstants.ENCODER_PULSE_TO_METERS);
+
         setLeftGains(Constants.leftGains);
         setRightGains(Constants.rightGains);
+
+        withEachMotor((m) -> m.burnFlash());
 
         REVGains.putGainsToBeTunedOnDash((getName() + "_RIGHT"), Constants.rightGains, getRightPIDFC());
         REVGains.putGainsToBeTunedOnDash((getName() + "_LEFT"), Constants.leftGains, getLeftPIDFC());

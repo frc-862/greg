aa358983351709ded0e8cceef908bf500129766f
Fritz Geib <github-f@standland.org>
2021-01-13 22:54:31 -0500

    Import Greg code into 2021 Wpilib

--- src/main/java/frc/robot/robots/GregContainer.java
+++ src/main/java/frc/robot/robots/GregContainer.java
@@ -7,13 +7,13 @@
 
 package frc.robot.robots;
 
-import edu.wpi.first.hal.sim.PCMSim;
 import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.simulation.PCMSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
